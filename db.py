597099c3e0de63c4d1b7a3085cb59fc6d5a5a0a2
AnyaIs <129720919+AnyaIs@users.noreply.github.com>
2024-05-12 18:34:25 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,24 +1,46 @@
-import uvicorn
-from fastapi import FastAPI, Response, Path, Query, Body, Header
-from fastapi.responses import HTMLResponse, PlainTextResponse, JSONResponse, FileResponse
-import db
-from public.router_users import users_router,classes_router
-
-app = FastAPI()
+from config import settings
+from sqlalchemy import Column, String, Integer, Sequence, insert, select
+from sqlalchemy import create_engine, text
+from models.good import Base, User, Classes
 
 
-db.create_tables()
-db.populate_classes_table()
-db.f_bilder()
+ur_s = settings.POSTGRES_DATABASE_URLS
+#ur_a = settings.POSTGRES_DATABASE_URLA
+#ur_p = 'postgresql://postgres:1234@localhost:5432/dbfastapi'
+print(ur_s)
 
-app.include_router(users_router)
-app.include_router(classes_router)
-
-
-@app.get("/")
-def main():
-    return FileResponse("files/index.html")
+engine_s = create_engine(ur_s, echo=True)
 
 
-if __name__ == "__main__":
-   uvicorn.run(app,host="127.0.0.1", port=8000)
+def create_tables():
+    Base.metadata.drop_all(bind=engine_s)
+    Base.metadata.create_all(bind=engine_s)
+
+
+def f_bilder():
+    with engine_s.connect() as conn:
+        query_class_ids = select(Classes.id, Classes.class_name)
+        class_ids = conn.execute(query_class_ids).fetchall()
+
+        query = insert(User).values([
+            {"name": "Pavel Pavlov", "hashed_password": "123545", "class_id": class_ids[0][0]},
+            {"name": "Ivan Ivanov", "hashed_password": "123545", "class_id": class_ids[1][0]}
+        ])
+        conn.execute(query)
+        conn.execute(text('commit;'))
+        query = select(User)
+        answer = conn.execute(query)
+        print(f"answer = {answer.all()}")
+
+
+def populate_classes_table():
+    with engine_s.connect() as conn:
+        query = insert(Classes).values([
+            {"class_name": "1215i"},
+            {"class_name": "1205u"}
+        ])
+        conn.execute(query)
+        conn.execute(text('commit;'))
+        query = select(Classes)
+        answer = conn.execute(query)
+        print(f"answer = {answer.all()}")
