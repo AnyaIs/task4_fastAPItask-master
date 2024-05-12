a79fe51dad8753e761e31f8ab4ec0ccd687c2c31
AnyaIs <129720919+AnyaIs@users.noreply.github.com>
2024-05-12 19:12:08 +0700

    Update good.py

--- models/good.py
+++ models/good.py
@@ -1,18 +1,9 @@
-from typing import Union, Annotated
+from typing import Union, List
 from pydantic import BaseModel, Field, HttpUrl
 from sqlalchemy import Column, String, Integer, Identity, Sequence, Float, Boolean, ForeignKey, MetaData
 from sqlalchemy.orm import declarative_base
 
-from enum import Enum
-
 Base = declarative_base()
-
-
-
-
-
-
-
 
 class User(Base):
     __tablename__ = "users"
@@ -20,60 +11,42 @@ class User(Base):
     name = Column(String, index=True, nullable=False)
     hashed_password = Column(String)
 
-    class Tags(Enum):
-        users = "users"
-        advents = "advents"
-        info = "info"
-        good = "good"
+class Tags(str, Enum):
+    users = "users"
+    advents = "advents"
+    info = "info"
+    good = "good"
 
 class Person(BaseModel):
- lastName: str = Field(defailt="lastname", min_length=3, max_length=20)
- age: int = Field(default=100, ge=10, lt=200)
+    lastName: str = Field(default="lastname", min_length=3, max_length=20)
+    age: int = Field(default=100, ge=10, lt=200)
 
 class Foto(BaseModel):
- url: HttpUrl
-name: Union[str, None] = None
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    url: HttpUrl
+    name: Union[str, None] = None
 
 class User_new(BaseModel):
-     name: Union[str, None] = None
-     id: Annotated[Union[int, None], Field(default=100, ge=10, lt=200)] = None
-     person: Union[Person, None] = None
-     day_list0: list
-     day_list1: Union[list, None] = None
-     day_list2: Union[list[int], None] = None
-     foto_list: Union[list[Foto], None] = None
+    name: Union[str, None] = None
+    id: int = Field(default=100, ge=10, lt=200)
+    person: Union[Person, None] = None
+    day_list0: List[str]
+    day_list1: Union[List[str], None] = None
+    day_list2: Union[List[int], None] = None
+    foto_list: Union[List[Foto], None] = None
 
 class Good(BaseModel):
-         id: Annotated[Union[int, None], Field(default=100, ge=1, lt=200)] = None
-         name: Union[str, None] = None
-         description: Union[str, None] = None
-         price: Union[float, None] = 0
-         nalog: Union[float, None] = 13.6
+    id: int = Field(default=100, ge=1, lt=200)
+    name: Union[str, None] = None
+    description: Union[str, None] = None
+    price: Union[float, None] = 0
+    nalog: Union[float, None] = 13.6
 
 class Main_User(BaseModel):
-             id: Annotated[Union[int, None], Field(default=100, ge=1, lt=200)] = None
-             name: Union[str, None] = None
-
+    id: int = Field(default=100, ge=1, lt=200)
+    name: Union[str, None] = None
 
 class Main_UserDB(Main_User):
-                 hashed_password: Annotated[Union[str, None], Field(max_length=200, min_length=6)] = None
-
+    hashed_password: str = Field(max_length=200, min_length=6)
 
 class New_Respons(BaseModel):
-                     message: str
+    message: str
