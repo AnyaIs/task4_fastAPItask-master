2c10d91213fb24afa2dd06c09895007702569386
AnyaIs <129720919+AnyaIs@users.noreply.github.com>
2024-05-12 18:18:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,37 @@
 from dotenv import load_dotenv
 import os
-from pydantic_settings import BaseSettings
 
 
-# class Settings(BaseSettings):
-#     app_name: str
-#     DB_HOST:str
-#     DB_PORT:int
-#     DB_NAME:str
-#     DB_USER:str
-#     DB_PASS:str
-
+class Settings:
+    POSTGRES_DATABASE_URLS: str = "postgresql://postgres:postgres@localhost:5432/db"
+    POSTGRES_DATABASE_URLA: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/db"
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
 
 
 load_dotenv()
+settings = Settings()
 
-DB_HOST=os.environ.get("DB_HOST")
-DB_PORT=int(os.environ.get("DB_PORT"))
-DB_NAME=os.environ.get("DB_NAME")
-DB_USER=os.environ.get("DB_USER")
-DB_PASS=os.environ.get("DB_PASS")
+#settings.POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+settings.POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', 5432))
+settings.POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+settings.POSTGRES_USER = os.environ.get('POSTGRES_USER')
+settings.POSTGRES_DB = os.environ.get('POSTGRES_DB')
+settings.POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+
+settings.POSTGRES_DATABASE_URLS = f"postgresql:" \
+                                  f"//{settings.POSTGRES_USER}:" \
+                                  f"{settings.POSTGRES_PASSWORD}" \
+                                  f"@{settings.POSTGRES_HOST}:" \
+                                  f"{settings.POSTGRES_PORT}" \
+                                  f"/{settings.POSTGRES_DB}"
+
+settings.POSTGRES_DATABASE_URLA = f"postgresql+asyncpg:" \
+                                  f"//{settings.POSTGRES_USER}:" \
+                                  f"{settings.POSTGRES_PASSWORD}" \
+                                  f"@{settings.POSTGRES_HOST}:" \
+                                  f"{settings.POSTGRES_PORT}" \
+                                  f"/{settings.POSTGRES_DB}"
